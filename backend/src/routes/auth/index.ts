9768bbbb23535deff6fde3813f60ef35cb0ea19f
Shunto Ishiguro <shunto.ishiguro.job@gmail.com>
2025-11-27 17:49:09 +0900

    /auth/register のルートを追加し、新規登録APIを実装

--- backend/src/routes/auth/index.ts
+++ backend/src/routes/auth/index.ts
@@ -1,11 +1,13 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors';
 import loginRouter from './login'
+import registerRouter from './register'
 
 const auth = new Hono()
 
 auth.use('*', cors());
 
 auth.route('/login', loginRouter)
+auth.route('/register', registerRouter)
 
 export default auth
